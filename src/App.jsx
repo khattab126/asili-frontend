652199f63d7c6504f503609681996ccb92b33b1c
Khattab126 <youssefahmedkhattab126@gmail.com>
2026-02-16 13:06:59 +0200

    Fix case sensitivity: import Navbar from Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import Navbar from "./Components/navbar_ui/navbar";
+import Navbar from "./Components/navbar_ui/Navbar";
 import About from "./pages/About";
 import Collection from "./pages/Collection";
 import Home from "./pages/Home";
